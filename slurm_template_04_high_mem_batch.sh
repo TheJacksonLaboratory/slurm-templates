7fe81f6e54dea98f81e19d4f1c397758aa493fb8
kurtshowmaker <kurtshowmaker@gmail.com>
2022-06-13 22:19:17 -0400

    added notes

--- slurm_template_04_high_mem_batch.sh
+++ slurm_template_04_high_mem_batch.sh
@@ -7,7 +7,7 @@
 #SBATCH --ntasks=1           # Do not change unless you know what your doing (it sets the number of tasks (do not change for non-mpi jobs))
 #SBATCH --cpus-per-task=46   # Set the number of CPUs for task (change to number of CPU/threads utilized in run script/programs) [limited to 142 CPUs per node]
 #SBATCH --mem=1024GB         # Here set to ~1TB (~22GB per core set above )[limited to 3022GB per node]
-#SBATCH --time=24:00:00      # Set the max time limit (batch QoS 72 hr limit, long QoS 336 hr limit)
+#SBATCH --time=24:00:00      # Set the max time limit (high_mem partition has 72 hr limit)
 
 ###-----load modules if needed-------###
 module load singularity
