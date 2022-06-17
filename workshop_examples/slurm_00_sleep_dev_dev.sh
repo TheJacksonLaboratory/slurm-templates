f4dcf4ac632b5f510e7d9c27aa0edc942db103e0
kurtshowmaker <kurtshowmaker@gmail.com>
2022-06-17 01:44:34 -0400

    added workshop examples

---
+++ workshop_examples/slurm_00_sleep_dev_dev.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#SBATCH --job-name=MY_JOB    # Set job name
+#SBATCH --partition=dev      # Set the partition 
+#SBATCH --qos=dev            # Set the QoS
+#SBATCH --nodes=1            # Do not change unless you know what your doing (it set the nodes (do not change for non-mpi jobs))
+#SBATCH --ntasks=1           # Do not change unless you know what your doing (it sets the number of tasks (do not change for non-mpi jobs))
+#SBATCH --cpus-per-task=1    # Set the number of CPUs for task (change to number of CPU/threads utilized in run script/programs) [limited to 30 CPUs per node]
+#SBATCH --mem=1GB            # Set to a value ~10-20% greater than max amount of memory the job will use (or ~6 GB per core, for dev) (limited to 180 GB per node on compute partition)
+#SBATCH --time=1:00:00       # Set the max time limit (dev partition/QoS has 8 hr limit)
+
+###-----load modules if needed-------###
+
+
+###-----run script below this line-------###
+
+sleep 20
+
---
+++ workshop_examples/slurm_02_array_dev_dev.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#SBATCH --job-name=MY_JOB    # Set job name
+#SBATCH --partition=dev      # Set the partition 
+#SBATCH --qos=dev            # Set the QoS
+#SBATCH --nodes=1            # Do not change unless you know what your doing (it set the nodes (do not change for non-mpi jobs))
+#SBATCH --ntasks=1           # Do not change unless you know what your doing (it sets the number of tasks (do not change for non-mpi jobs))
+#SBATCH --cpus-per-task=1    # Set the number of CPUs for task (change to number of CPU/threads utilized in run script/programs) [limited to 30 CPUs per node]
+#SBATCH --mem=6GB            # Set to a value ~10-20% greater than max amount of memory the job will use (or ~6 GB per core, for dev) (limited to 180 GB per node on compute partition)
+#SBATCH --time=1:00:00       # Set the max time limit (dev partition/QoS has 8 hr limit)
+#SBATCH --array=1-4          # runs array, no limit to how many run  
+
+###-----load modules if needed-------###
+
+
+###-----run script below this line-------###
+
+LIB=$(sed -n -e "${SLURM_ARRAY_TASK_ID} p" /fastscratch/${USER}/adir/slurm-templates/my_important_list.txt)
+
+echo "${LIB}_R1.fq    ${LIB}_R2.fq" > array_run2_${SLURM_ARRAY_TASK_ID}_out_${LIB}_file_names.txt
