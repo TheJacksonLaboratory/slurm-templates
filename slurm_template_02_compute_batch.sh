55d49453648ebc880c949071ee4247ff70739ef1
Kurt Showmaker <36570582+kurtshowmaker@users.noreply.github.com>
2022-06-17 07:52:07 -0400

    Update slurm_template_02_compute_batch.sh

--- slurm_template_02_compute_batch.sh
+++ slurm_template_02_compute_batch.sh
@@ -5,7 +5,7 @@
 #SBATCH --qos=batch          # Set the QoS
 #SBATCH --nodes=1            # Do not change unless you know what your doing (it set the number of nodes (do not change for non-mpi jobs))
 #SBATCH --ntasks=1           # Do not change unless you know what your doing (it sets the number of tasks (do not change for non-mpi jobs))
-#SBATCH --cpus-per-task=4    # Set the number of CPUs for task (change to number of CPU/threads utilized in run script/programs) [limited to 70 CPUs per node]
+#SBATCH --cpus-per-task=4    # Set the number of CPUs for task (change to number of CPU/threads utilized) [limited to 70 CPUs per node]
 #SBATCH --mem=48GB           # Set to a value ~10-20% greater than max amount of memory the job will use (or ~10 GB per core, for compute partition) [limited to 760 GB per node on compute partition]
 #SBATCH --time=72:00:00      # Set the max time limit (batch QoS 72 hr limit)
 
